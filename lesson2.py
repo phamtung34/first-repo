3cdb4c200314452e675d015f2939520ebb4dddfc
unknown <phamtung4030@gmail.com>
2022-06-21 11:11:56 +0700

    first commit

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+# name = input("Nhap ten: ")
+# print("Xin chào %s " %name)
+
+
+# print(4000+1000-3000)
+# print(12 * 3 / 4)
+
+# name = 'Python'
+# year = 2000
+# print(name + ' was born in ' + str(year))
+
+name = input("Nhập tên ai đó: ")
+print("Xin chào bạn %s nhé." % name)
